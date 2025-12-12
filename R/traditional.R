77c142a3a277b5afdd1339f4aecc512c21fb0262
Ron Wehrens <ron.wehrens@wur.nl>
2025-12-12 15:05:37 +0100

    fixed a small bug in the traditional way of determining PoIs - has to do with NA values

--- R/traditional.R
+++ R/traditional.R
@@ -34,7 +34,8 @@ tradT2M <- function(y, x = 1:length(y)) {
     ntime <- length(y)
     max.idx <- which.max(y)
 
-    if ((max.idx > 1 && is.na(y[max.idx - 1])) ||
+    if (is.na(max.idx) ||
+        (max.idx > 1 && is.na(y[max.idx - 1])) ||
         (max.idx < ntime && is.na(y[max.idx + 1]))) {
 
         NA
